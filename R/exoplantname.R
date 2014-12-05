31ef0a99967d846ebe3820ae3f2d5c8b39c6801a
Kyle Hamilton <kyle.hamilton@gmail.com>
2014-12-04 19:24:59 -0800

    Corrected spelling mistake

--- R/exoplantname.R
+++ R/exoplantname.R
@@ -1,28 +1,28 @@
 #' Exoplanet Name
 #'
-#' This will return infomation about the requested exoplant, 
-#' @title Search for exoplant infomation
-#' @param exoplant a string of an existing exoplant
-#' @return JSON object with infomation about the queried exoplant
+#' This will return infomation about the requested exoplanet, 
+#' @title Search for exoplanet infomation
+#' @param exoplanet a string of an existing exoplanet
+#' @return JSON object with infomation about the queried exoplanet
 #' @keywords Name
 #' @examples
 #' \dontrun{
 #' library(RCurl)
 #' library(RJSONIO)
 #' ###Return Infomation about the exoplanet 11 Com
-#' exoplantname("11 Com")
+#' exoplanetname("11 Com")
 #' ###Return Infomation about the exoplanet Kepler-9
-#' exoplantname("Kepler-9")
+#' exoplanetname("Kepler-9")
 #' ###Return Infomation about all exoplants in the system
-#' exoplantname("")
+#' exoplanetname("")
 #' }
 #' @export
 
-exoplantname <- function (exoplant) {
+exoplanetname <- function (exoplanet) {
   internetcheck <- url.exists("http://star-api.herokuapp.com", timeout = 10)
   if( internetcheck != TRUE)
     stop('Hacktheuniverse or your internet connection is down')
-  urldata <- paste('http://star-api.herokuapp.com/api/v1/exo_planets/', URLencode(exoplant), sep = "")
+  urldata <- paste('http://star-api.herokuapp.com/api/v1/exo_planets/', URLencode(exoplanet), sep = "")
   data <- getURL(urldata)
   dataFrame <- RJSONIO::fromJSON(data)
   return (dataFrame)
